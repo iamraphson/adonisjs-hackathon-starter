16541be0c9b8aad40004ee26f35947d8ac834c5b
Ayeni Olusegun <nsegun5@gmail.com>
2017-01-27 20:20:53 +0100

    feat(AHS):implement newyork times api

--- app/Http/Controllers/NewYorkTimesController.js
+++ app/Http/Controllers/NewYorkTimesController.js
@@ -3,12 +3,42 @@
  */
 const ApiRepository = make('App/Repositories/ApiRepository')
 const Env = use('Env')
-const async = require('async');
+const request = require('request');
 
 class NewYorkTimesController {
 
-    constructor() {
+    constructor () {
+        this.query = {
+            'list-name': 'young-adult',
+            'api-key': Env.get('NEWYORKTIMES_BOOK_ID')
+        }
+    }
 
+    * index (request, response) {
+        try {
+            const nytResponse = yield this.getData()
+            yield response.sendView('api.nytApi', { books: nytResponse.books })
+        } catch(e){
+            console.log(e.message);
+            yield response.sendView('api.nytApi', { books: [] })
+        }
+    }
+
+    getData() {
+        return new Promise((resolve, reject) => {
+            request.get({ url: 'http://api.nytimes.com/svc/books/v2/lists', qs: this.query },
+                (err, request, body) => {
+                    if (err) { return reject(err); }
+
+                    if (request.statusCode === 403) {
+                        return reject(new Error('Invalid New York Times API Key'));
+                    }
+
+                    return resolve({
+                        books: JSON.parse(body).results
+                    })
+            });
+        })
     }
 }
 
