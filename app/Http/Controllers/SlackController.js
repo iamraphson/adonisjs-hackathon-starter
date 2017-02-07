164a5bd34ac1c56a17df47e028f88091f763763b
Ayeni Olusegun <nsegun5@gmail.com>
2017-02-07 05:09:07 +0100

    get all the members of a group on slack and display their info

--- app/Http/Controllers/SlackController.js
+++ app/Http/Controllers/SlackController.js
@@ -1,9 +1,32 @@
 'use strict'
 
 const Env = use('Env')
+const WebClient = require('@slack/client').WebClient;
 class SlackController {
-	* index(request, response) {
 
+	constructor(){
+		this.token = Env.get('SLACK_TOKEN')
+		this.web = new WebClient(this.token)
+	}
+
+	* index(request, response) {
+		try {
+			const slackResponse = yield this.getAllUsersInATeam()
+			yield response.sendView('api.slackApi', {members: slackResponse.members})
+		} catch(e){
+			console.log(e);
+			yield response.sendView('api.slackApi', {members: []})
+		}
+  }
+
+  getAllUsersInATeam(){
+		return new Promise((resolve, reject) => {
+			this.web.users.list({}, (err, users) => {
+				if (err) { return  reject(err); }
+				console.log(users)
+				return resolve({members: users.members})
+			})
+		})
   }
 }
 
