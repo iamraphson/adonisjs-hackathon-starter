f27da75218018a57d0b2a5e3bcd44b590eb3d848
Ayeni Olusegun <nsegun5@gmail.com>
2017-07-16 13:32:34 +0100

    fix middleware issues

--- app/Http/Middleware/ViewAssets.js
+++ app/Http/Middleware/ViewAssets.js
@@ -1,14 +1,14 @@
 'use strict'
 
-const View = use('View')
-
 class ViewAssets {
 
   * handle (request, response, next) {
     let baseUrl = request.secure() ? 'https://' : 'http://'
     baseUrl += request.headers().host + '/'
 
-    View.global('assets', (assetPath) => {
+	  response.viewInstance = use('View')
+
+	  response.viewInstance.global('assets', (assetPath) => {
       assetPath = (assetPath.substring(0, 1) == '/') ? assetPath.substring(1) : assetPath
       return baseUrl + assetPath
     })
--- app/Http/Middleware/ViewUrl.js
+++ app/Http/Middleware/ViewUrl.js
@@ -1,14 +1,14 @@
 'use strict'
 
-const View = use('View')
-
 class ViewUrl {
 
   * handle (request, response, next) {
     let baseUrl = request.secure() ? 'https://' : 'http://'
     baseUrl += request.headers().host + '/'
 
-    View.global('url', (path) => {
+	  response.viewInstance = use('View')
+
+	  response.viewInstance.global('url', (path) => {
       if (typeof path !== 'undefined') {
         path = (path.substring(0, 1) == '/') ? path.substring(1) : path
         return baseUrl + path
