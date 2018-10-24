72eb80f9280f6679738106524ede96150a41f387
Ayeni Olusegun <nsegun5@gmail.com>
2018-10-24 18:34:08 +0100

    re-format hook.jd

--- start/hooks.js
+++ start/hooks.js
@@ -2,9 +2,10 @@
 
 const { ioc } = require('@adonisjs/fold')
 const { hooks } = require('@adonisjs/ignitor')
+const bitbucket = use('App/Extensions/Bitbucket')
 
 hooks.before.providersRegistered(() => {
   ioc.extend('Adonis/Addons/Ally', 'bitbucket', function () {
-    return require('../app/Extensions/Bitbucket')
+    return bitbucket
   })
 })
