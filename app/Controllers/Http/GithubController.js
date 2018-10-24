e869e3a74967d6591ddd0afd8543b3daaf0ccc26
Ayeni Olusegun <nsegun5@gmail.com>
2018-10-24 17:30:06 +0100

    fix bugs with github api

--- app/Controllers/Http/GithubController.js
+++ app/Controllers/Http/GithubController.js
@@ -3,8 +3,7 @@
 const octokit = require('@octokit/rest')()
 const api = make('App/Services/ApiService')
 class GithubController {
-  async index ({request, response, auth, view}) {
-
+  async index ({ request, response, auth, view }) {
     try {
       let user = await auth.getUser()
       const token = await api.getToken('github', user.id)
@@ -14,10 +13,10 @@ class GithubController {
 
       try {
         const githubResponse = await this.getGithubProfile()
-        return view.render('api.github', {repo: githubResponse.data})
+        return view.render('api.github', { repo: githubResponse.data })
       } catch (e) {
         console.log(e.message)
-        return view.render('api.github', {repo: {}})
+        return view.render('api.github', { repo: {} })
       }
     } catch (e) {
       console.log(e)
@@ -26,11 +25,7 @@ class GithubController {
   }
 
   async getGithubProfile () {
-    const result = await octokit.repos.get({ owner: 'iamraphson', repo: 'adonisjs-hackathon-starter' })
-    const result2 = await octokit.repos.getForUser({username: 'iamraphson', type: 'type'})
-
-    console.log(result2.length)
-    return result
+    return await octokit.repos.get({ owner: 'iamraphson', repo: 'adonisjs-hackathon-starter' })
   }
 }
 
